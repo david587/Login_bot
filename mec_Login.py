1a54c973a8510592f478870db90d586d46479426
David Barath <davidbarath08@gmail.com>
2022-11-06 17:36:43 +0100

    Cleaning code

--- mec_Login.py
+++ mec_Login.py
@@ -9,13 +9,15 @@ cookies = mechanize.CookieJar()
 br.set_cookiejar(cookies)
 br.addheaders = [('User-agent','Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36')]
 br.set_handle_refresh(False)
-br.open("https://afeladat2017.whiteshield.net/login")
+a=br.open("https://afeladat2017.whiteshield.net/login")
+print("open:",a.read())
 br.select_form(nr=0)
 
 res = br.submit();
 html = res.read()
 soup = BeautifulSoup(html,"lxml")
 form = soup.find("form", action="/login")
+print(form.text)
 math= form.find("span", id="math").text
 math_input = form.find("input", id="id_captcha_1")  
 
@@ -40,7 +42,8 @@ br.form["captcha_1"] = str_operator
 Cap.append(first_math)
 Cap.append(web_operators)
 Cap.append(second_math)
-
+print(str_operator)
+print(br.form)
 response = br.submit()
 
 print("submit:",response.read())
